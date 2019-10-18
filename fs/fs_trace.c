a4fab5f102e541942ec522da4e45b78f061114bf
mraveendrababu <raveendra@sigtuple.com>
2019-10-18 18:48:14 +0530

    dcache tracing is added

--- fs/fs_trace.c
+++ fs/fs_trace.c
@@ -27,6 +27,7 @@ int fs_annon_inode_trace_enable=0;
 int fs_block_dev_trace_enable=0;
 int fs_sync_trace_enable=0;
 int fs_mbcache_trace_enable=0;
+int fs_dcache_trace_enable=0;
 
 EXPORT_SYMBOL(fs_trace_enable);
 EXPORT_SYMBOL(fs_open_trace_enable);
@@ -53,6 +54,7 @@ EXPORT_SYMBOL(fs_block_dev_trace_enable);
 EXPORT_SYMBOL(fs_annon_inode_trace_enable);
 EXPORT_SYMBOL(fs_sync_trace_enable);
 EXPORT_SYMBOL(fs_mbcache_trace_enable);
+EXPORT_SYMBOL(fs_dcache_trace_enable);
 
 
 int fs_trace_dummy(void){
